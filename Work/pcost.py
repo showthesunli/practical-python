be77070ae9a4f93ad8a788e3d2c00a2e2d05a2e4
lee <showthesunli@gmail.com>
2020-11-28 01:18:13 +0800

    practice 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,10 +1,11 @@
 # pcost.py
 #
-# Exercise 1.27
-with open('Data/portfolio.csv', 'rt') as f:
-    next(f)
-    cost = 0
-    for line in f:
-        row = line.split(',')
-        cost = cost+ int(row[1])* float(row[2])
-    print(f'total cost {cost}')
+# Exercise 1.30
+def protfolio_cost(name):
+    with open(name, 'rt') as f:
+        next(f)
+        cost = 0
+        for line in f:
+            row = line.split(',')
+            cost = cost+ int(row[1])* float(row[2])
+    return cost
