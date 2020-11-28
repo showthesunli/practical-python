7ebb74f036888f17c6d1c8dc15a19f4092d5af30
lee <showthesunli@gmail.com>
2020-11-29 07:00:49 +0800

    practice 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,19 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                portfolio.append((row[0], int(row[1]), float(row[2])))
+            except ValueError as identifier:
+                pass
+            
+    return portfolio
+
+if '__main__' == __name__:
+    print(read_portfolio('Data/portfolio.csv'))
