27b007a5112552fabd7c424120e6dd616accd18d
lee <leeorz@prototype-PC.localdomain>
2020-11-30 09:06:15 +0800

    exercise 2.7

--- Work/report.py
+++ Work/report.py
@@ -1,19 +1,44 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.7
 import csv
+import pprint
 
 def read_portfolio(filename):
     portfolio = []
-    with open(filename) as f:
+    with open(filename, 'rt') as f:
         rows = csv.reader(f)
         for row in rows:
             try:
-                portfolio.append((row[0], int(row[1]), float(row[2])))
+                portfolio.append({'name':row[0],
+                 'shares':int(row[1]), 'price':float(row[2])})
             except ValueError as identifier:
                 pass
             
     return portfolio
 
+def read_prices(filename):
+    """
+    read stock prices to dict from prices.csv
+    """
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except Exception as identifier:
+                pass
+    return prices
+
 if '__main__' == __name__:
-    print(read_portfolio('Data/portfolio.csv'))
+    # print(read_portfolio('Data/portfolio.csv'))
+    # print(read_portfolio('Data/portfolio.csv'))
+    # print(read_prices('Data/prices.csv'))
+    portfolio = read_portfolio('Data/portfolio.csv')
+    prices = read_prices('Data/prices.csv')
+    intres = 0.00
+    for i in portfolio:
+        intres_of_one_share = prices[i['name']]*i['shares'] - i['shares']*i['price']
+        intres += intres_of_one_share
+    print('total gain is', intres)
