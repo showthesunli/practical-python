60aac737454f65c010bda201620d5d52452553ea
lee <showthesunli@gmail.com>
2020-12-04 06:35:37 +0800

    exercise 3.3

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,15 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+def parse_csv(filename: str) -> list:
+    records = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            record = dict(zip(header, row))
+            records.append(record)
+    return records
