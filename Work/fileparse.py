20c0bed94a1e02ff22776c7b6038c07e123456d2
lee <showthesunli@gmail.com>
2020-12-04 08:11:39 +0800

    exercise 3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,31 +1,30 @@
 # fileparse.py
 #
-# Exercise 3.5
+# Exercise 3.7
 
 import csv
 
-def parse_csv(filename: str, select: list = None, types: list = None, hashead = True) -> list:
+def parse_csv(filename: str, select: list = None, types: list = None, hashead = True, delimiter = ',') -> list:
     records = [] 
     indes = []
     with open(filename, 'rt') as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter = delimiter)
+
         header = next(rows) if hashead else []
 
         if select:
             indes = [header.index(key) for key in select]
             header = select
+
         for row in rows:
             record = row
-
             if indes:
                 record = [row[i] for i in indes]
             if types:
                 record = [func(val) for func, val in zip(types, row)]
-            
             if hashead:
                 records.append(dict(zip(header, record)))
             else:
-
                 records.append(tuple(record))
     return records
      
